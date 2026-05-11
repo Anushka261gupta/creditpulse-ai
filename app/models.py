3346302d31411af85a32dc1ad40523bb74157358
Anushka261gupta <151006503+Anushka261gupta@users.noreply.github.com>
2026-05-11 23:11:08 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,33 +5,4 @@ class EmailResponse(BaseModel):
     subject: str
     body: str
 
-def parse_email_response(email_text):
 
-    subject = ""
-    body = ""
-
-    lines = email_text.split("\n")
-
-    for i, line in enumerate(lines):
-
-        if "subject" in line.lower():
-
-            subject = line.replace(
-                "**",
-                ""
-            ).replace(
-                "Subject:",
-                ""
-            ).replace(
-                "1. Subject:",
-                ""
-            ).strip()
-
-            body = "\n".join(lines[i+1:]).strip()
-
-            break
-
-    return EmailResponse(
-        subject=subject,
-        body=body
-    )
