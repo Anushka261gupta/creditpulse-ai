edde1872f85f97d39b967134f0d1e2a308861b10
Anushka261gupta <151006503+Anushka261gupta@users.noreply.github.com>
2026-05-11 23:16:57 +0530

    Update email_agent.py

--- app/email_agent.py
+++ app/email_agent.py
@@ -1,11 +1,14 @@
-
 from groq import Groq
 
 from config import GROQ_API_KEY
 from prompts import build_email_prompt
 from models import EmailResponse
 
-client = Groq(api_key=GROQ_API_KEY)
+
+client = Groq(
+    api_key=GROQ_API_KEY
+)
+
 
 def parse_email_response(email_text):
 
@@ -29,14 +32,17 @@ def parse_email_response(email_text):
                 ""
             ).strip()
 
-            body = "\n".join(lines[i+1:]).strip()
+            body = "\n".join(
+                lines[i + 1:]
+            ).strip()
 
             break
 
-return EmailResponse(
-    subject=subject,
-    body=body
-)
+    return EmailResponse(
+        subject=subject,
+        body=body
+    )
+
 
 def generate_email(
     client_name,
@@ -55,24 +61,32 @@ def generate_email(
     )
 
     response = client.chat.completions.create(
+
         model="llama-3.1-8b-instant",
+
         messages=[
+
             {
                 "role": "system",
-                "content": "You are a professional finance collection assistant."
+
+                "content":
+                "You are a professional finance collection assistant."
             },
+
             {
                 "role": "user",
+
                 "content": prompt
             }
         ],
+
         temperature=0.4
     )
 
     email_text = response.choices[0].message.content
 
-    parsed_email = parse_email_response(email_text)
+    parsed_email = parse_email_response(
+        email_text
+    )
 
     return parsed_email
-
-
